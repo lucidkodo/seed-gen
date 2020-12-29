e87dd04feeb8bf16776c3bb6861a24836a3f35f8
angie -o <angie@compass-interactive.com>
2020-12-29 19:53:09 +0800

    Update to pug & sass

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+img(alt="Vue logo" src="./assets/logo.png")
+HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
@@ -14,13 +14,12 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
 </style>
