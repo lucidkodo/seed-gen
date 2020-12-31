c3d7e9557d186b8ef404f869a359036e12595c8b
angie -o <angie@compass-interactive.com>
2020-12-31 22:11:21 +0800

    WIP

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,26 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  data () {
+    return {
+      target: '',
+
+      randomize () {
+        console.log('Let\'s go!')
+      }
+    }
+  },
+
+  mounted () {
+    // load data
+    this.randomize()
   }
+
 }
 </script>
 
 <style lang="sass">
-#app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
+@import app
 </style>
