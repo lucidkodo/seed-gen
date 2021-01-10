0b06bb74ef2e0c7a4e362df06e006c1671e230fc
angie -r <lucidkodo@gmail.com>
2021-01-11 06:27:37 +0800

    Open in new tab

--- src/copy.vue
+++ src/copy.vue
@@ -4,7 +4,7 @@ p Empty databases?
 p Use this to generate the data for your projects! #[br] Click #[b Generate Data] to view sample!
 
 #do
-  p Project hosted on DigitalOcean #[a(href="https://www.digitalocean.com/docs/app-platform/") App Platform]!
+  p Project hosted on DigitalOcean #[a(href="https://www.digitalocean.com/docs/app-platform/", target="_blank") App Platform]!
   p Visit #[a(href="https://github.com/lucidkodo/seed-gen", target="_blank") repo] for source code.
   a(href="https://cloud.digitalocean.com/apps/new?repo=https://github.com/lucidkodo/seed-gen/tree/main")
     img(
